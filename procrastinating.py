6c7ef671d9ebe2c253b77b09e5eb00cf7847c212
Maggie Tseng <mjtseng42@gmail.com>
2018-05-06 21:43:06 -0400

    small edits and a to-do

--- procrastinating.py
+++ procrastinating.py
@@ -1,10 +1,14 @@
+
+# Procrastinating Fiesta:
 # are you procrastinating?
+
 notworking = input("Are you procrastinating? Type 'True' or 'False' ")
 # likely answers
 if (str(notworking) == "True"):
 	print ("Shame on you. You have things to do. Sad!")
 elif (str(notworking) == "False"):
 	print ("gfy... Are you lying? Because you (very likely) have work.")
+
 # easter eggs
 elif(str(notworking) == "42"):
 	print ("I pity the fool that procrastinates... but good answer")
@@ -13,4 +17,9 @@ elif(str(notworking) == "Geico could save you 15% or more on car insurance"):
 else:
 	print ("Sorry, I didn't get that. (You're procrastinating right now, aren't you?)")
 
-#TODO: make this into something useful or at least more entertaining
+#TODOS: 
+# make this into something useful or at least more entertaining
+# try to optimize code - there has to be a better way to do this
+
+# it would be cool if there was a log of some sort, of how often you were/weren't procrastinating.
+# maybe have an array to keep track of dates as well? (the day and a list of that day's results?) idk really
