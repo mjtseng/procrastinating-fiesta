da94e7ca772005df5b42488052ce1cb00dbb0606
Maggie Tseng <mjtseng42@gmail.com>
2018-05-09 21:53:22 -0400

    added list of functions to be used

--- procrastinating.py
+++ procrastinating.py
@@ -3,6 +3,7 @@
 # are you procrastinating?
 
 # may want to make this into a function instead?
+# also, u sure you want to use bools?
 notworking = input("Are you procrastinating? Type 'True' or 'False' ")
 # likely answers
 if (str(notworking) == "True"):
@@ -17,6 +18,7 @@ elif(str(notworking) == "Geico could save you 15% or more on car insurance"):
 	print ("Can't argue with that.")
 else:
 	print ("Sorry, I didn't get that. (You're procrastinating right now, aren't you?)")
+	# or, you can make a temporary variable to store the answer in, and then you can put it in the log later!
 
 '''
 LOG CODE:
@@ -35,7 +37,7 @@ def add_entry:
 	else:
 		print ("Response discarded.")
 	print ("Press space to go again or x to exit! To clear your log, press c.")
-# pseudocode, idk the syntax, and does this go in the function add_entry ?
+# pseudocode, idk the syntax - hopefully this exists, and does this go in the function add_entry ?
 if (space == pressed):
 	# call notworking function again
 	notworking()
@@ -46,19 +48,19 @@ elif (x == pressed):
 
 '''
 
-
-#TODOS: 
-# make this into something useful or at least more entertaining
+#TODOS and NOTES:
 # try to optimize code - there has to be a better way to do this
 
 # it would be cool if there was a log of some sort, of how often you were/weren't procrastinating.
 # maybe have an array to keep track of dates as well? (the day and a list of that day's results?) idk really
-	# ok so having days sounds hard and I'm going to focus on a list of results for now
+	# ok so having days sounds hard and I'm going to focus on a list of results for now (as of 5/9/18)
 
-# MASTER LIST OF ALL FUNCTIONS because I can't keep track of them:
 '''
-- 
-- 
-- 
+MASTER LIST OF ALL FUNCTIONS because I can't keep track of them and what they do:
+
+- notworking() is used to take the user's initial input of whether they are procrastinating at the moment.
+- add_entry() is used to put the user's entry into the log if they opt to do so.
+- clear_log() is used to delete all entries from the log and allow the user to put their new input into a newly blank log.
+	- should it be clearlog and add entry, or just clear log without adding an entry? Maybe I can put an option here.
 
 '''
