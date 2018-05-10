817a50e090705d0323432c19be05878806f1301b
Maggie Tseng <mjtseng42@gmail.com>
2018-05-09 21:44:55 -0400

    added to log pseudocode

--- procrastinating.py
+++ procrastinating.py
@@ -22,19 +22,27 @@ else:
 LOG CODE:
 
 # create log here: list or something else mutable.
-wannalog = input("Do you want to log your answer for the future? (y/n)")
-if (wannalog == "y"):
-	print("Logging...")
-	# put result into records
-	print("Logged!")
-else:
-	print ("Response discarded.")
-print ("Press space to go again or x to exit!")
-# pseudocode, idk the syntax:
-# if (space == pressed):
+log
+
+# create function to add an entry to the log
+
+def add_entry:
+	wannalog = input("Do you want to log your answer for the future? (y/n)")
+	if (wannalog == "y"):
+		print("Logging...")
+		# put result into records
+		print("Logged!")
+	else:
+		print ("Response discarded.")
+	print ("Press space to go again or x to exit! To clear your log, press c.")
+# pseudocode, idk the syntax, and does this go in the function add_entry ?
+if (space == pressed):
 	# call notworking function again
-# elif (x == pressed):
-	# exit program
+	notworking()
+elif (c == pressed):
+	# clear entire log (will need another function for this?)
+elif (x == pressed):
+	# exit whole program
 
 '''
 
@@ -45,3 +53,12 @@ print ("Press space to go again or x to exit!")
 
 # it would be cool if there was a log of some sort, of how often you were/weren't procrastinating.
 # maybe have an array to keep track of dates as well? (the day and a list of that day's results?) idk really
+	# ok so having days sounds hard and I'm going to focus on a list of results for now
+
+# MASTER LIST OF ALL FUNCTIONS because I can't keep track of them:
+'''
+- 
+- 
+- 
+
+'''
